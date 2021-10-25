c7e30112e78537c416be3089cc91de40d2bcc881
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-10-25 09:58:30 -0300

    feat: ensure requests return json as default

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -4,7 +4,7 @@ import app from '../config/app'
 describe('Cors Middleware', () => {
   test('Should enable cors', async () => {
     app.post('/test_cors', (req, res) => {
-      res.send(req.body)
+      res.send()
     })
     await request(app).get('/test_cors')
       .expect('access-control-allow-origin', '*')
