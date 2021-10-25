6e2647c13bb0e5f69653452e9c1a5c1c2d6fb273
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-10-25 11:44:15 -0300

    feat: add express router adapter

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,7 +2,7 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import SignUpController from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 
 export const makeSignUpController = (): SignUpController => {
   const emailValidatorAdapter = new EmailValidatorAdapter()
