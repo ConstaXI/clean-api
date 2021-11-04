0d9f4de1e6756b4b620d52f19ce72fe3d62fc27f
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-11-04 08:45:16 -0300

    feat: ensure a survey will be added on database

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,42 @@
+import { Collection } from 'mongodb'
+import MongoHelper from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+
+const makeFakeSurveyData = (): AddSurveyModel => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }, {
+    answer: 'another_answer'
+  }]
+})
+
+let surveyCollection: Collection
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect('mongodb://localhost:27017/jest')
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add(makeFakeSurveyData())
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
